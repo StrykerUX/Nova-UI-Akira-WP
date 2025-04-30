76e913704e294727f9f87a68b73af9714f920b00
StrykerUX <abraham.stryker117@gmail.com>
2025-04-29 22:16:00 -0600

    Return v0.2.41 Stable
    Regreso a versión antes de la implementación de colores v1

--- functions.php
+++ functions.php
@@ -75,7 +75,6 @@ function nova_scripts() {
     // Theme CSS
     wp_enqueue_style('nova-app', NOVA_TEMPLATE_URI . '/assets/css/app.min.css', array('bootstrap'), NOVA_VERSION);
     wp_enqueue_style('nova-theme', NOVA_TEMPLATE_URI . '/assets/css/theme.min.css', array('nova-app'), NOVA_VERSION);
-    wp_enqueue_style('nova-icon-hover', NOVA_TEMPLATE_URI . '/assets/css/icon-hover-effects.css', array('nova-theme'), NOVA_VERSION);
     
     // Custom Logo Styles
     wp_enqueue_style('nova-logo-styles', NOVA_TEMPLATE_URI . '/assets/css/logo-styles.css', array('nova-app'), NOVA_VERSION);
@@ -95,7 +94,6 @@ function nova_scripts() {
     // Theme JS
     wp_enqueue_script('nova-layout', NOVA_TEMPLATE_URI . '/assets/js/layout.js', array('jquery'), NOVA_VERSION, true);
     wp_enqueue_script('nova-theme', NOVA_TEMPLATE_URI . '/assets/js/theme.js', array('jquery'), NOVA_VERSION, true);
-    wp_enqueue_script('nova-icon-hover', NOVA_TEMPLATE_URI . '/assets/js/icon-hover.js', array('jquery'), NOVA_VERSION, true);
 
     // Main JS
     wp_enqueue_script('nova-app', NOVA_TEMPLATE_URI . '/assets/js/app.js', array('jquery', 'bootstrap-bundle', 'simplebar'), NOVA_VERSION, true);
@@ -167,11 +165,6 @@ require NOVA_TEMPLATE_DIR . '/inc/template-functions.php';
  * Customizer additions
  */
 require NOVA_TEMPLATE_DIR . '/inc/customizer.php';
-
-/**
- * Advanced color customization
- */
-require NOVA_TEMPLATE_DIR . '/inc/color-customizer.php';
 
 /**
  * Custom comment template
